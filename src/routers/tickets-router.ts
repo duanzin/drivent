6f8b96b30c5a1800730a7eaafb85e0961a2a71d8
duanzin <lucaduan@gmail.com>
2023-04-17 12:23:07 -0300

    fix: tickets router

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -4,8 +4,8 @@ import { authenticateToken } from '@/middlewares';
 
 const ticketsRouter = Router();
 
-ticketsRouter.get('/', authenticateToken, getTicketTypes);
-ticketsRouter.get('/types', authenticateToken, getUserTickets);
+ticketsRouter.get('/types', authenticateToken, getTicketTypes);
+ticketsRouter.get('/', authenticateToken, getUserTickets);
 ticketsRouter.post('/', authenticateToken, createUserTicket);
 
 export { ticketsRouter };
